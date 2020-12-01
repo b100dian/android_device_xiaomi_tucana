ef8890269beee44950133f3157e3b8dd8d9743f5
alibei <sebastiankotzias83@gmail.com>
2020-12-01 14:41:24 +0100

    tucana: debug on eng build

--- product_prop.mk
+++ product_prop.mk
@@ -1,3 +1,12 @@
+# ADB
+ifeq ($(TARGET_BUILD_VARIANT),eng)
+# /vendor/default.prop is force-setting ro.adb.secure=1
+# Get rid of that by overriding it in /product on eng builds
+PRODUCT_PRODUCT_PROPERTIES += \
+    ro.secure=0 \
+    ro.adb.secure=0
+endif
+
 # Recorder
 PRODUCT_PRODUCT_PROPERTIES += \
     persist.vendor.audio.fluence.voicerec=true \
